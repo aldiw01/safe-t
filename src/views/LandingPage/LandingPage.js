728138c245ae64a80eff0bf3a9488b3b03e16c7c
ankyaditya <ankyaditya17@gmail.com>
2019-10-16 15:14:14 +0700

    testing front end violation data 14

--- src/views/LandingPage/LandingPage.js
+++ src/views/LandingPage/LandingPage.js
@@ -75,12 +75,12 @@ class LandingPage extends React.Component {
                 <Button
                   color="warning"
                   size="lg"
+                  href="/violation"
                   // target="_blank"
                   rel="noopener noreferrer"
                 >
-                  
                   {/* <Link to="/violation"> </Link> */}
-                  <i className="fab fa-warning" /> <Link to="/violation">Violation Data</Link>
+                  <i className="fab fa-warning" /> Violation Data
                 </Button>
               </GridItem>
             </GridContainer>
