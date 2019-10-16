d6a31ddce0f4de31345245e29601d321cc39b740
ankyaditya <ankyaditya17@gmail.com>
2019-10-16 15:10:33 +0700

    testing front end violation data 13

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ const ResetPassword = React.lazy(() => import('./views/Pages/ResetPassword'));
 const Page404 = React.lazy(() => import('./views/Pages/Page404'));
 const Page500 = React.lazy(() => import('./views/Pages/Page500'));
 const LandingPage = React.lazy(() => import('./views/LandingPage/LandingPage'));
+const ViolationPage = React.lazy(() => import('./views/ViolationPage/ViolationPage'));
 
 class App extends Component {
 
@@ -36,6 +37,7 @@ class App extends Component {
             <Route exact path="/404" name="Page 404" render={props => <Page404 {...props} />} />
             <Route exact path="/500" name="Page 500" render={props => <Page500 {...props} />} />
             <Route exact path="/home" name="Landing Page" render={props => <LandingPage {...props} />} />
+            <Route exact path="/violation" name="Violation Page" render={props => <ViolationPage {...props} />} />
             <Route path="/" name="Home" render={props => <DefaultLayout {...props} />} />
           </Switch>
         </React.Suspense>
