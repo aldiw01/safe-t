eee809918adbd33448cb438a471e0c4bde697804
ankyaditya <ankyaditya17@gmail.com>
2019-10-17 15:15:25 +0700

    test paginate 6

--- src/views/ViolationPage/Sections/ViolationSection.jsx
+++ src/views/ViolationPage/Sections/ViolationSection.jsx
@@ -132,9 +132,9 @@ class ViolationSection extends React.Component {
     var newItems = []
     const menuItems = this.mapToAlphaGrid()
     for (let index = 12 * (counter - 1); index < (12 * counter); index++) {
-      console.log("menu items index"+menuItems[index])
+      console.log("menu items index"+Object.keys(menuItems)[index])
       newItems.push(
-        menuItems[index]
+        Object.keys(menuItems)[index]
       )
     }
     console.log("new items"+newItems)
